821a64b0b03266eb578da93fb29840ee47e29864
MHaseeb55555 <muhammadhaseeb.55555@gmail.com>
2023-04-02 01:15:41 +0500

    work complete

---
+++ java.js
@@ -0,0 +1,61 @@
+// alert('muhammad haseeb');
+
+var a = ' Name : Muhammad Haseeb';
+
+alert(a);
+
+var id;
+id = " CNIC : 42301-1234567-9";
+alert(id);
+
+var c = 12;
+var b = c + 3;
+console.log(b);
+
+var d = 15
+var e = 5 + d;
+console.log(e);
+
+var whatsLeftOver = 9 % 3;
+
+console.log(whatsLeftOver);
+
+var num = 1;
+
+z = --num + 1;
+
+console.log(z);
+console.log(num);
+
+var q = 'thanks,';
+var w = ' haseeb';
+var e = ' !';
+alert(q + ' muhammad' + w + e);
+
+var r = prompt(' enter your first name');
+var t = prompt(' age');
+var u = ' city';
+var i = 'karachi';
+var y = prompt(u, i);
+
+alert(r + ' ' + t + ' from ' + y);
+console.log(r + ' ' + t + ' from ' + y);
+
+
+var numberOfCats = prompt("How many cats?");
+var tooManyCats = numberOfCats;
+alert(' there are ' + tooManyCats + ' cats');
+
+
+
+var x = prompt('what is your name', 'haseeb')
+if (x == 'haseeb') {
+    alert('ok')
+}
+else {
+    alert('incorrect name')
+}
+
+
+
+
